31bbac275b0cd440d0b30b9e7c4e4c404ce6d66f
Jimena León Huertas <mena.leon@estudiantec.cr>
2022-09-19 09:45:24 -0600

    Condicionales en SintacticAnalyzer

--- src/SintacticAnalyzer.py
+++ src/SintacticAnalyzer.py
@@ -17,25 +17,21 @@ precedence = (
 )
 
 
-# PENDIENTE 1: definir Scopes para las funciones y que asi el parser me reconozca N cantidad de instrucciones dentro de las funciones
-# PENDIENTE 2: definir qué es una 'expresion' para incluirla en las condicionales booleanas y en las funciones que utilizan esas condicionales
 # PENDIENTE 3: modificar el archivo del IDE para que el Lexer ya no sea una clase
 # PENDIENTE 4: incluir el token TEXTVALUE (valores de texto, o sea strings) para probarlo en la funcion PrintValues (SIN ESA FUNCION EL PROYECTO VALE CERO)
+
 
 # RECONOCIMIENTO DE FUNCIONES Y LOS PROC ---------------------------------------------
 
 def p_Proced(p):        # Reconoce los Procedimientos, es decir los: Proc @nombre();
-    '''Proced : Proc PRINCIPAL LPARENTHESIS recursivo RPARENTHESIS SEMMICOLOM
-              '''
+    '''Proced : Proc ID LPARENTHESIS recursivo RPARENTHESIS SEMMICOLOM'''
     print("Proced ", p[4])
     # p[0]=Proced(p[1],"Proced")
+
 def p_recursivo(p):
     '''recursivo : recursivo instruccion
-                    | instruccion'''
+                 | instruccion'''
     pass
-
-
-
 
 def p_instruccion(p): # Una instruccion puede ser cualquiera de las sig. funciones
     '''instruccion : varDeclaration 
@@ -51,33 +47,36 @@ def p_instruccion(p): # Una instruccion puede ser cualquiera de las sig. funcion
                      | Until_f
                      | While_f
                      | Case_f
+                     | Case2_f
                      | PrintValues_f
+                     | call_f
                      | empty'''
     p[0] = p[1]
     print("instruccion ", p[1])    
 
 def p_Values_f(p):      # Cambia el valor de una variable
     '''Values_f : Values LPARENTHESIS ID COMMA valorDato RPARENTHESIS SEMMICOLOM
-                | LPARENTHESIS ID COMMA Alter_f RPARENTHESIS SEMMICOLOM'''
+                | LPARENTHESIS ID COMMA Alter_f RPARENTHESIS SEMMICOLOM
+                | LPARENTHESIS ID COMMA AlterB_f RPARENTHESIS SEMMICOLOM'''
     print("Values_f", p[2])
 
-def p_Alter_f(p):       # Altera ligeramente el valor de una variable
+def p_Alter_f(p):       # Altera ligeramente el valor de una variable y retorna dicho valor nuevo
     '''Alter_f : Alter LPARENTHESIS ID COMMA ADD COMMA valorDato RPARENTHESIS SEMMICOLOM
                | Alter LPARENTHESIS ID COMMA SUB COMMA valorDato RPARENTHESIS SEMMICOLOM
                | Alter LPARENTHESIS ID COMMA MUL COMMA valorDato RPARENTHESIS SEMMICOLOM
                | Alter LPARENTHESIS ID COMMA DIV COMMA valorDato RPARENTHESIS SEMMICOLOM'''
     print("Alter_f ", p[4])
 
-def p_AlterB_f(p):      # Altera el valor de una variables booleana (Alter_f pero solo con True y False)
+def p_AlterB_f(p):      # Altera el valor de una variables booleana (Alter_f pero solo con True y False) y retorna dicho valor nuevo
     '''AlterB_f : AlterB LPARENTHESIS ID RPARENTHESIS SEMMICOLOM'''
     print("ALterB_f ", p)
 
 def p_MoveRight_f(p):
-    '''MoveRight_f : MoveRight '''
+    '''MoveRight_f : MoveRight SEMMICOLOM'''
     print("MoveRight_f", p)
 
 def p_MoveLeft_f(p):
-    '''MoveLeft_f : MoveLeft '''
+    '''MoveLeft_f : MoveLeft SEMMICOLOM'''
     print("MoveLeft_f", p)
 
 def p_Hammer_f(p):
@@ -91,29 +90,48 @@ def p_Stop_f(p):
     '''Stop_f : Stop SEMMICOLOM'''
     print("Stop_f", p)
 
-def p_IsTrue_f(p):
-    '''IsTrue_f : IsTrue ID SEMMICOLOM'''
-    print("IsTrue_f", p)
-
 def p_Repeat_f(p):
-    '''Repeat_f : Repeat LPARENTHESIS instruccion Break SEMMICOLOM RPARENTHESIS SEMMICOLOM'''
+    '''Repeat_f : Repeat LPARENTHESIS recursivo Break RPARENTHESIS SEMMICOLOM'''
     print("Repeat_f", p)
 
 def p_Until_f(p):
-    '''Until_f : Until LPARENTHESIS instruccion RPARENTHESIS SEMMICOLOM'''
+    '''Until_f : Until LPARENTHESIS recursivo RPARENTHESIS condicional SEMMICOLOM'''
     print("Until_f", p)
 
 def p_While_f(p):
-    '''While_f : While LPARENTHESIS instruccion RPARENTHESIS SEMMICOLOM'''
+    '''While_f : While condicional LPARENTHESIS recursivo RPARENTHESIS SEMMICOLOM'''
     print("While_f", p)
 
 def p_Case_f(p):
-    '''Case_f : Case When LPARENTHESIS RPARENTHESIS Then LPARENTHESIS instruccion RPARENTHESIS SEMMICOLOM
-              | Case When LPARENTHESIS RPARENTHESIS Then LPARENTHESIS instruccion RPARENTHESIS Else LPARENTHESIS instruccion RPARENTHESIS SEMMICOLOM'''
+    '''Case_f : Case When LPARENTHESIS condicional RPARENTHESIS Then LPARENTHESIS recursivo RPARENTHESIS SEMMICOLOM
+              | Case When LPARENTHESIS condicional RPARENTHESIS Then LPARENTHESIS recursivo RPARENTHESIS Else LPARENTHESIS recursivo RPARENTHESIS SEMMICOLOM'''
     print("Case_f ", p)
 
+def p_opciones(p): # otra funcion recursiva para permitir varios "When ... Then"
+    '''opciones : opciones When valorDato Then LPARENTHESIS recursivo RPARENTHESIS
+                | When valorDato Then LPARENTHESIS recursivo RPARENTHESIS'''
+
+def p_Case2_f(p):
+    '''Case2_f : Case ID opciones SEMMICOLOM
+                | Case ID opciones Else recursivo SEMMICOLOM '''
+
+# something engloba a los ID de variables, valores de texto entre comillas(QUOTES) y numeros. 
+# El PrintValues puede imprimir varias cosas a la vez, por eso se incluye la posiblidad de poner coma (,)"
+
+#                  | QUOTES TEXTVALUE QUOTES COMMA something
+ #                | QUOTES TEXTVALUE QUOTES                          incluir esas 2 cuando ya sirva el token TEXTVALUE y QUOTES
+
+def p_something(p): 
+    '''something : ID COMMA something
+                 | expresionNum COMMA something
+                 | expresionNum
+                 | Number COMMA something
+                 | Number
+                 | empty'''
+
 def p_PrintValues_f(p):
-    '''PrintValues_f : PrintValues LPARENTHESIS RPARENTHESIS SEMMICOLOM'''
+    '''PrintValues_f : PrintValues LPARENTHESIS something RPARENTHESIS SEMMICOLOM'''
+
     print("PrintValues ", p)
 
 def p_empty(p):
@@ -121,7 +139,47 @@ def p_empty(p):
     print("Empty! ", p)
     pass
 
+# RECONOCIMIENTO DE CONDICIONALES ------------------------------------------
+        # estas son importantes para definir condiciones de finalización en funciones como: Until, While y Case When
 
+def p_condicional_f(p):
+    '''condicional : IsTrue_f
+                   | Greater_f
+                   | GreaterEqual_f
+                   | Equals_f
+                   | Different_f
+                   | LessEqual_f
+                   | Less_f'''
+    print("condicional ", p)
+
+def p_IsTrue_f(p):
+    '''IsTrue_f : IsTrue ID SEMMICOLOM'''
+    print("IsTrue_f", p)
+
+def p_Greater_f(p):
+    '''Greater_f : expresionNum Greater expresionNum SEMMICOLOM'''
+    print("Greater_f", p)
+
+def p_GreaterEqual_f(p):
+    '''GreaterEqual_f : expresionNum GreaterEqual expresionNum SEMMICOLOM'''
+    print("GreaterEqual", p)
+
+def p_Equals_f(p):
+    '''Equals_f : expresionNum Equals expresionNum SEMMICOLOM'''
+    print("Equals", p)
+
+def p_Different_f(p):
+    '''Different_f : expresionNum Different expresionNum SEMMICOLOM'''
+    print("Different_f:", p)
+
+def p_LessEqual_f(p):
+    '''LessEqual_f : expresionNum LessEqual expresionNum SEMMICOLOM'''
+    print("LessEquals", p)
+
+def p_Less_f(p):
+    '''Less_f : expresionNum Less expresionNum SEMMICOLOM'''
+    print("Less_f :", p)
+    
 
 # RECONOCIMIENTO DE VARIABLES---------------------------------------------------------
 
@@ -129,6 +187,11 @@ def p_varDeclaration(p):
     '''varDeclaration : New ID COMMA LPARENTHESIS tipoDatos COMMA valorDato RPARENTHESIS SEMMICOLOM '''
     #p[0] = var(p[5], p[7], "var")
     print("varDeclaration ", p[2])
+
+def p_expresionNum(p): # una expresion numerica puede ser el valor de una variable o el resultado de la funcion Alter
+    '''expresionNum : ID
+                     | Alter_f '''
+    print("expresionNum ", p[1])
 
 def p_tipoDatos(p):
     '''tipoDatos : Bool
@@ -143,15 +206,11 @@ def p_valorDato(p):
     #p[0] = p[1]
     print("valorDato ", p[1])
 
-# definir qué es una expresion 
-""""def p_expresion(p):
-    '''expresion : '''
-"""
 
 # LLAMAR A UNA FUNCIÓN ---------------------------------------------------------------
 
 def p_call_f(p):
-    '''call : CALL LPARENTHESIS ID RPARENTHESIS SEMMICOLOM'''
+    '''call_f : CALL LPARENTHESIS ID RPARENTHESIS SEMMICOLOM'''
     print("call_f ", p)
 
 # RECONOCIMIENTO DE ERRORES LÉXICOS --------------------------------------------------
@@ -168,7 +227,7 @@ fp = codecs.open(file)
 cadena = fp.read()
 parser = yacc.yacc()
 fp.close()
-result=parser.parser(cadena)
+result=parser.parse(cadena)
 #result.imprimir("")
 #print result
 """
